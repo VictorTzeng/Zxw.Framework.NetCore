c8dd5b80001a47172d92c5f26e9ad8ecd4c9c26a
zengxw <victor.tzeng@outlook.com>
2019-01-08 22:04:33 +0800

    update packages

---
+++ Zxw.Framework.Pay/PayBase.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using ICanPay.Core;
+
+namespace Zxw.Framework.Pay
+{
+    public abstract class PayBase<TGateWay> :IPay 
+        where TGateWay : GatewayBase
+    {
+        private IGateways _gateways;
+
+        public virtual GatewayBase Gateway => _gateways.Get<TGateWay>();
+
+        protected PayBase(IGateways getGateways)
+        {
+            _gateways = getGateways;
+        }
+        public virtual void BillDownload(IAuxiliary auxiliary)
+        {
+            Gateway.BillDownload(auxiliary);
+        }
+
+        public virtual string CreateOrder(IOrder order, GatewayTradeType tradeType, Action<object, PaymentSucceedEventArgs> succeed = null, Action<object, PaymentFailedEventArgs> failed = null)
+        {
+            if (tradeType == GatewayTradeType.Barcode)
+            {
+                if (succeed == null)
+                {
+                    throw new ArgumentNullException(nameof(succeed));
+                }
+                if (failed == null)
+                {
+                    throw new ArgumentNullException(nameof(failed));
+                }
+                Gateway.PaymentSucceed += succeed;
+                Gateway.PaymentFailed += failed;
+            }
+            return Gateway.Payment(order);
+        }
+
+        public virtual INotify Cancel(IAuxiliary auxiliary)
+        {
+            return Gateway.Cancel(auxiliary);
+        }
+
+        public virtual INotify Close(IAuxiliary auxiliary)
+        {
+            return Gateway.Close(auxiliary);
+        }
+
+        public virtual INotify Query(IAuxiliary auxiliary)
+        {
+            return Gateway.Query(auxiliary);
+        }
+
+        public virtual INotify Refund(IAuxiliary auxiliary)
+        {
+            return Gateway.Refund(auxiliary);
+        }
+
+        public virtual INotify RefundQuery(IAuxiliary auxiliary)
+        {
+            return Gateway.RefundQuery(auxiliary);
+        }
+    }
+}
---
+++ Zxw.Framework.Pay/UnionPay/UnionPay.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using ICanPay.Core;
+using ICanPay.Unionpay;
+
+namespace Zxw.Framework.Pay.UnionPay
+{
+    /// <summary>
+    /// 银联支付
+    /// </summary>
+    public class UnionPay:PayBase<UnionpayGateway>
+    {
+        public UnionPay(IGateways getGateways) : base(getGateways)
+        {
+        }
+
+        /// <summary>
+        /// 银联对账单下载
+        /// </summary>
+        /// <param name="billType"></param>
+        /// <param name="billDate"></param>
+        public void BillDownload(string billType, string billDate)
+        {
+            base.BillDownload(new Auxiliary() { BillDate = billDate, TxnTime = billType });
+        }
+
+        /// <summary>
+        /// 下单
+        /// </summary>
+        /// <param name="order"></param>
+        /// <param name="tradeType"></param>
+        /// <param name="succeed"></param>
+        /// <param name="failed"></param>
+        /// <returns></returns>
+        public string CreateOrder(Order order, GatewayTradeType tradeType,
+            Action<object, PaymentSucceedEventArgs> succeed = null,
+            Action<object, PaymentFailedEventArgs> failed = null)
+        {
+            return base.CreateOrder(order, tradeType, succeed, failed);
+        }
+
+        /// <summary>
+        /// 取消订单
+        /// </summary>
+        /// <param name="tradeNo"></param>
+        /// <param name="outRefundNo"></param>
+        /// <param name="refundAmount"></param>
+        /// <returns></returns>
+        public Notify Cancel(string tradeNo, string outRefundNo, double? refundAmount)
+        {
+            return (Notify)base.Cancel(new Auxiliary()
+            {
+                TradeNo = tradeNo,
+                OutRefundNo = outRefundNo,
+                TxnTime = DateTime.Now.ToString("yyyyMMddHHmmss"),
+                RefundAmount = refundAmount
+            });
+        }
+
+        /// <summary>
+        /// 查询订单
+        /// </summary>
+        /// <param name="orderNo"></param>
+        /// <returns></returns>
+        public Notify Query(string orderNo)
+        {
+            return (Notify)base.Query(new Auxiliary()
+            {
+                OutTradeNo = orderNo
+            });
+        }
+        /// <summary>
+        /// 订单退款
+        /// </summary>
+        /// <param name="orderNo"></param>
+        /// <returns></returns>
+        public Notify Refund(string orderNo)
+        {
+            return (Notify)base.Refund(new Auxiliary()
+            {
+                OutTradeNo = orderNo
+            });
+        }
+        /// <summary>
+        /// 订单退款查询
+        /// </summary>
+        /// <param name="orderNo"></param>
+        /// <param name="refundNo"></param>
+        /// <returns></returns>
+        public Notify RefundQuery(string orderNo, string refundNo)
+        {
+            return (Notify)base.RefundQuery(new Auxiliary()
+            {
+                OutTradeNo = orderNo,
+                OutRefundNo = refundNo
+            });
+        }
+    }
+}
